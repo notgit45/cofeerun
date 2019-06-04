ce10a7db3821fc605b6b7566ae7ee9fa58a0fe29
notgit45 <ser8584@ya.ru>
2019-06-04 21:15:44 +0700

    Page 295

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,3 +1,11 @@
-(function (window) {
-    'use strict';
-})();
+(function(Window) {
+  "use strict";
+  var App = window.App || {};
+
+  function DataStore() {
+    console.log("running the DataStore fuNction");
+  }
+
+  App.DataStore = DataStore;
+  window.App = App;
+})(window);
