f9b41cb8c2a624b58b80cc6fd96a33414f266114
notgit45 <ser8584@ya.ru>
2019-06-17 22:19:22 +0700

    Page 228

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,10 +1,26 @@
-(function(Window) {
-  "use strict";
+"use strict";
+
+(function (window) {
   var App = window.App || {};
 
   function DataStore() {
-    console.log("running the DataStore fuNction");
+    //    console.log("running the DataStore fuNction");
+    this.data = {};
   }
+
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  };
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  };
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  };
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
+  };
+
 
   App.DataStore = DataStore;
   window.App = App;
