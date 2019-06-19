26bf977e494e9b9b181abc583262e43d69a7a1ae
notgit45 <ser8584@ya.ru>
2019-06-19 22:24:10 +0700

    Page 254

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,12 +1,34 @@
 (function (window) {
     'use strict';
+
     var App = window.App || {};
     var $ = window.jQuery;
-    function FormHandler() {
+
+    function FormHandler(selector) {
         if (!selector) {
             throw new Error('No selector provided');
         }
+        this.$formElement = $(selector);
+        if (this.$formElement.length === 0) {
+            throw new Error('Coukd not find element with selector: ' + selector);
+        }
     }
-    var FormHandler = App.FormHandler;
+
+    FormHandler.prototype.addSubmitHandler = function (fn) {
+        console.log('Setting submit handler for form');
+        this.$formElement.on('submit', function (event) {
+            event.preventDefault();
+            var data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            fn(data);
+            this.reset();
+            this.elements[0].focus();
+        })
+    }
+    App.FormHandler = FormHandler;
     window.App = App;
 })(window);
